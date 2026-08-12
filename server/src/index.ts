2717cb38b3e1d0f0b135b563aedf3a96fc7491a5
Kirakit Kingkaew <zerofolk338@gmail.com>
2026-08-12 17:28:13 +0700

    Revert "Feature/1 project foundation"

--- server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app.js";
-
-const PORT = Number(process.env.PORT) || 3000;
-
-app.listen(PORT, () => {
-  console.log(`TokTickIT API listening on http://localhost:${PORT}`);
-});
